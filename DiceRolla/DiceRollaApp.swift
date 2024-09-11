84c3c3d1ec6005b40bc2e91f1ba2c4e9f2ee416b
Marcus Benoit <benoit.marcus@gmail.com>
2024-09-11 11:11:27 +0200

    Initial Commit

---
+++ DiceRolla/DiceRollaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollaApp.swift
+//  DiceRolla
+//
+//  Created by Marcus Benoit on 11.09.24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
